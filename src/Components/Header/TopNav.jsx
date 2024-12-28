ba34bd0e8a493ea59e676eb78d7f32bb94ca0ddf
hvs1234 <harshvardhansharma17@gmail.com>
2024-12-28 19:42:32 +0530

    added language translator

--- src/Components/Header/TopNav.jsx
+++ src/Components/Header/TopNav.jsx
@@ -11,7 +11,7 @@ const TopNav = () => {
     <>
       <div
         className="top-nav relative z-[9999] w-[100%] px-[20rem] py-[1rem] flex items-center justify-between gap-[2rem] 
-        max-xl:px-[5rem] max-md:px-[2rem] max-[750px]:flex-col max-[750px]:items-start"
+        max-xl:px-[5rem] max-md:px-[2rem] max-[850px]:flex-col max-[850px]:items-start"
       >
         <Link
           to={"/"}
@@ -24,24 +24,26 @@ const TopNav = () => {
             className="w-[50%] rounded-md brightness-[2]"
           />
         </Link>
-        <div className="flex items-center gap-[4rem] max-[400px]:flex-col max-[400px]:gap-[1rem] max-[400px]:items-start">
-          <div className="flex items-center">
-            <a
-              href="mailto:lekhakgaon309@gmail.com"
-              className="text-[1.6rem] text-[white] font-normal hover:opacity-[0.8] transition-all duration-[0.2s] ease-linear"
-            >
-              <i className="fa-solid fa-envelope rounded-full text-[1.6rem] bg-[#475579] text-[white] px-[0.8rem] py-[0.8rem]"></i>
-              &nbsp; lekhakgaon309@gmail.com
-            </a>
-          </div>
-          <div className="flex items-center gap-[0.5rem]">
-            <a
-              href="tel:9997277779"
-              className="text-[1.6rem] text-[white] font-normal hover:opacity-[0.8] transition-all duration-[0.2s] ease-linear"
-            >
-              <i className="fa-solid fa-phone rounded-full text-[1.6rem] bg-[#475579] text-[white] px-[0.8rem] py-[0.8rem]"></i>
-              &nbsp; +91-9997277779
-            </a>
+        <div className="flex items-center gap-[4rem] justify-end w-[100%] max-[850px]:justify-between">
+          <div className="flex items-center gap-[1rem] max-[500px]:flex-col max-[500px]:gap-[1rem] max-[500px]:items-start">
+            <div className="flex items-center">
+              <a
+                href="mailto:lekhakgaon309@gmail.com"
+                className="text-[1.6rem] text-[white] font-normal hover:opacity-[0.8] transition-all duration-[0.2s] ease-linear"
+              >
+                <i className="fa-solid fa-envelope rounded-full text-[1.6rem] bg-[#475579] text-[white] px-[0.8rem] py-[0.8rem]"></i>
+                &nbsp; lekhakgaon309@gmail.com
+              </a>
+            </div>
+            <div className="flex items-center gap-[0.5rem]">
+              <a
+                href="tel:9997277779"
+                className="text-[1.6rem] text-[white] font-normal hover:opacity-[0.8] transition-all duration-[0.2s] ease-linear"
+              >
+                <i className="fa-solid fa-phone rounded-full text-[1.6rem] bg-[#475579] text-[white] px-[0.8rem] py-[0.8rem]"></i>
+                &nbsp; +91-9997277779
+              </a>
+            </div>
           </div>
           <div className="">
             <select

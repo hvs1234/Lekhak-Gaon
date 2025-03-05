5e47a4985d98647708fcb707e7e5a90ed92c5250
hvs1234 <harshvardhansharma17@gmail.com>
2025-03-05 22:06:13 +0530

    add video on homepage

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -9,16 +9,16 @@ const Hero = () => {
 
   return (
     <>
-      <Header />
-      <div className="section-main w-[100%] relative object-cover h-[100vh] px-[20rem] py-[9rem] transition-all duration-[0.2s] ease-linear max-xl:px-[5rem] max-md:px-[2rem]">
+      <Header />  
+      <div className="section-main w-[100%] relative object-cover h-[70vh] px-[20rem] py-[9rem] transition-all duration-[0.2s] ease-linear max-xl:px-[5rem] max-md:px-[2rem]">
         <video
           className="absolute top-0 left-0 w-full h-full object-cover transition-all duration-[0.4s] ease-linear"
-          src="/Media/hero-video.mp4"
+          src="/Media/lekhakGaon-video.mp4"
           autoPlay
           loop
           muted
         ></video>
-        <div className="absolute top-0 left-0 w-[100%] h-[100%] bg-[#2525d1] opacity-[0.2]"></div>
+        <div className="absolute top-0 left-0 w-[100%] h-[100%] bg-[black] opacity-[0]"></div>
       </div>
     </>
   );
